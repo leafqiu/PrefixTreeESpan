9365e3bfec7af315f93849a8fc41a72a7b285808
leafqiu <leafqiu@example.com>
2018-11-03 21:58:11 +0800

    Try to find correct growth elements.

--- fortest.py
+++ fortest.py
@@ -1,16 +1,12 @@
-# import os
-#
-# if __name__ == '__main__':
-#     testnames = 'testdatas'
-#     inputs = [os.path.join('./', testnames, name) for name in ['F5.data', 'D10.data']]
-#     sup = [100, 1000, 10000]
-#     outnames = 'outputs'
-#     for name in inputs:
-#         for s in sup:
-#             out = os.path.basename(name).split('.')[0] + '-' + str(s) + '.out'
-#             output = os.path.join('./', outnames, out)
-#             os.system("python ./prefixtreeespan.py -i %s -o %s -s %d" % (name, output, s))
+import os
 
-list = list(range(5))
-for index, node in enumerate(list[5:], 5):
-    print(index, node)
+if __name__ == '__main__':
+    testnames = 'testdatas'
+    inputs = [os.path.join('./', testnames, name) for name in ['F5.data', 'D10.data']]
+    sup = [100, 1000, 10000]
+    outnames = 'outputs'
+    for name in inputs:
+        for s in sup:
+            out = os.path.basename(name).split('.')[0] + '-' + str(s) + '.out'
+            output = os.path.join('./', outnames, out)
+            os.system("python ./prefixtreeespan.py -i %s -o %s -s %d" % (name, output, s))
